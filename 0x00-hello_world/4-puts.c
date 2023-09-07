35322b9e77f6333fb1211d96b6e85d81b070b666
Ogomd <Ogomd>
2023-09-07 16:57:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * main - this is a main functio
+ * Return: always 0
  */
 int main(void)
 {
-puts(""Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
